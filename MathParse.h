1250b2edc41301c21eec300e55ba7c8d5e1750c8
Vishesh Meena <visheshmeena47@gmail.com>
2025-05-07 16:37:33 +0530

    Update MathParse.h

--- MathParse.h
+++ MathParse.h
@@ -8,6 +8,8 @@
 #include <utility>
 #include <cctype>
 
+namespace SSM{
+
 class expression{
   
   private:
@@ -814,8 +816,7 @@ class expression{
     expression(const std::string& input, bool allow_var = true): entered_expression(input), allow_variable(allow_var){}
 
     void clean(){
-      if(!this->anamoly) return;
-        
+      
       this->anamoly = false;
       
       //initialization
@@ -1032,7 +1033,8 @@ class expression{
       std::string line;
       std::getline(is, line);
       obj.entered_expression = line;
-      obj.clean();
       return is;
     }
 };
+
+}
