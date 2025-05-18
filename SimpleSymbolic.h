420175d9c97f76b2ce08a416524c8c98a0b64968
Vishesh Meena <visheshmeena47@gmail.com>
2025-05-18 22:58:34 +0530

    Update and rename simpleSymbolic.h to SimpleSymbolic.h

---
+++ SimpleSymbolic.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//cpp
+#include <unordered_map>
+#include <stdexcept>
+#include <functional>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+#include <stack>
+#include <utility>
+#include <cctype>
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+
+//custom
+#include "MathParse.h"
+#include "Poly.h"
+#include "NumericalDer.h"
--- simpleSymbolic.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "MathParse.h"
-#include "NumericalDer.h"
