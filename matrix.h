4248b67c0202f8ed7005482a2572da20ba6d630c
Vishesh Meena <visheshmeena47@gmail.com>
2025-07-16 23:46:19 +0530

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 
-static constexpr bool INDEX_OFFSET = true;
+namespace mat {
+
+static constexpr bool OFFSET_INDEX = true;
 
 // Matrces don't usually grow in size, so it will not be a wrapper on std::vector.
 template <typename T>
@@ -10,15 +12,7 @@ class RectangularMatrix {
     T* m_entries = nullptr;
     size_t m_rows = 0;
     size_t m_columns = 0;
-    
-    void destroy() noexcept {
-      if(m_entries) {
-        for(size_t i = 0; i < order(); i++) m_entries[i].~T();
-        delete[] m_entries;
-        m_entries = nullptr;
-      }
-    }
-    
+  
   public:
     // Dimension constructors.
     RectangularMatrix(size_t rows, size_t columns): m_rows(rows), m_columns(columns) {
@@ -29,13 +23,13 @@ class RectangularMatrix {
     // Dimesion constructor with defualt value.
     RectangularMatrix(size_t rows, size_t columns, const T &to_copy): m_rows(rows), m_columns(columns) {
       if(order() == 0) throw std::runtime_error("Any dimension of the matrix can't be 0.");
-      m_entries = new T[order()]();
+      m_entries = new T[order()];
       for(size_t i = 0; i < order(); i++) m_entries[i] = to_copy;
     }
     
     // Copy constructor.
     RectangularMatrix(const RectangularMatrix &other): m_rows(other.m_rows), m_columns(other.m_columns) {
-      m_entries = new T[other.order()]();
+      m_entries = new T[order()];
       for(size_t i = 0; i < order(); i++) m_entries[i] = other.m_entries[i];
     }
     
@@ -43,13 +37,14 @@ class RectangularMatrix {
     RectangularMatrix(RectangularMatrix &&other): m_rows(other.m_rows), m_columns(other.m_columns), m_entries(other.m_entries) {
       other.m_rows = 0; // Convert other to defualt.
       other.m_columns = 0;
-      other.m_entries = 0;
+      other.m_entries = nullptr;
     }
     
     RectangularMatrix& swap(RectangularMatrix &other) noexcept {
       std::swap(m_rows, other.m_rows);
       std::swap(m_columns, other.m_columns);
       std::swap(m_entries, other.m_entries);
+      return *this;
     }
     
     // Move assignment operator.
@@ -66,7 +61,7 @@ class RectangularMatrix {
       return *this;
     }
     
-    // No check for sizes unlike operator=().
+    // No check for sizes unlike operator=(), so the size can change if different sized matrix is used.
     RectangularMatrix& copy(const RectangularMatrix& other) {
       if(this == &other) return *this;
       RectangularMatrix temp(other);
@@ -78,24 +73,32 @@ class RectangularMatrix {
       return m_rows * m_columns;
     }
     
-    // Index starts from 1, instead of 0 accounting for general representation of matrix elements.
+    // Index starts from 1, instead of 0 accounting for general representation of matrix elements but can be changed by changing the boolean in this header file.
     T& at(size_t row, size_t column) {
-      if(INDEX_OFFSET && row * column == 0) throw std::runtime_error("Index of matrices start at 1,1. Please start with 1, not 0 (or change the offset to false in header file).");
-      if(row > m_rows + INDEX_OFFSET || column > m_columns + INDEX_OFFSET) throw std::runtime_error("Index used to access the elements can't be greater than the row/column present.");
-      return *(m_entries + m_columns * (row - INDEX_OFFSET) + (column - INDEX_OFFSET));
+      if(OFFSET_INDEX && row * column == 0) throw std::runtime_error("Index of matrices start at 1,1. Please start with 1, not 0 (or change the offset to false in header file).");
+      if(row > m_rows + OFFSET_INDEX || column > m_columns + OFFSET_INDEX) throw std::runtime_error("Index used to access the elements can't be greater than the row/column present.");
+      return *(m_entries + m_columns * (row - OFFSET_INDEX) + (column - OFFSET_INDEX));
     }
     
     const T& at(size_t row, size_t column) const {
-      if(INDEX_OFFSET && row * column == 0) throw std::runtime_error("Index of matrices start at 1,1. Please start with 1, not 0 (or change the offset to false in header file).");
-      if(row > m_rows + INDEX_OFFSET || column > m_columns + INDEX_OFFSET) throw std::runtime_error("Index used to access the elements can't be greater than the row/column present.");
-      return *(m_entries + m_columns * (row - INDEX_OFFSET) + (column - INDEX_OFFSET));
+      if(OFFSET_INDEX && row * column == 0) throw std::runtime_error("Index of matrices start at 1,1. Please start with 1, not 0 (or change the offset to false in header file).");
+      if(row > m_rows + OFFSET_INDEX || column > m_columns + OFFSET_INDEX) throw std::runtime_error("Index used to access the elements can't be greater than the row/column present.");
+      return *(m_entries + m_columns * (row - OFFSET_INDEX) + (column - OFFSET_INDEX));
     }
     
     // Destructor....RAII.
     ~RectangularMatrix() {
-      destroy();
+      delete[] m_entries;
     }
 };
+
+// To make swappable using std::swap.
+template<typename T>
+void swap(RectangularMatrix<T> &lfs, RectangularMatrix<T> &rhs) noexcept {
+  lfs.swap(rhs);
+}
+
+}
 
 int main() {
   RectangularMatrix<std::string> my_matrix(2,1,"Hello");
