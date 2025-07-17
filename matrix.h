7396505ab3dd9f5cd065c14d53d09d079008f971
Vishesh Meena <visheshmeena47@gmail.com>
2025-07-17 22:10:20 +0530

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -17,7 +17,8 @@ class RectangularMatrix {
     T* m_entries = nullptr;
     size_t m_rows = 0;
     size_t m_columns = 0;
-    
+  
+  // Constructors and move semantics.
   public:
     // Dimension constructors.
     RectangularMatrix(size_t rows, size_t columns): m_rows(rows), m_columns(columns) {
@@ -75,16 +76,18 @@ class RectangularMatrix {
       other.m_entries = nullptr;
     }
     
+    // Move assignment operator.
+    RectangularMatrix& operator=(RectangularMatrix &&other) noexcept {
+      swap(other);
+      return *this;
+    }    
+    
+  // Non-constructor assignment methods/operators.
+  public:
     RectangularMatrix& swap(RectangularMatrix &other) noexcept {
       std::swap(m_rows, other.m_rows);
       std::swap(m_columns, other.m_columns);
       std::swap(m_entries, other.m_entries);
-      return *this;
-    }
-    
-    // Move assignment operator.
-    RectangularMatrix& operator=(RectangularMatrix &&other) noexcept {
-      swap(other);
       return *this;
     }
     
@@ -104,6 +107,8 @@ class RectangularMatrix {
       return *this;
     }
     
+  // Filling operations.
+  public:
     // Fill the matrix with a single element copied in the full matrix.
     RectangularMatrix& fill(const T &to_fill) noexcept {
       for(size_t i = 0; i < n_entries(); i++) m_entries[i] = to_fill;
@@ -122,6 +127,8 @@ class RectangularMatrix {
       return *this;
     }
     
+  // Getters.
+  public:
     // Provides the number of rows present.
     size_t n_rows() const noexcept {
       return m_rows;
@@ -137,6 +144,13 @@ class RectangularMatrix {
       return m_rows * m_columns;
     }
     
+    // Const pointer to the underlying data array. Only location(const).
+    const T* data() const noexcept {
+      return m_entries;
+    }
+    
+  // Matrix information methods(information related to a matrix).
+  public:
     // Returns the actual order of the matrix, i.e, m x n.
     std::pair<size_t, size_t> order() const noexcept {
       return {m_rows, m_columns};
@@ -167,15 +181,47 @@ class RectangularMatrix {
       return *(m_entries + m_columns * (row - OFFSET_INDEX) + (column - OFFSET_INDEX));
     }
     
+    // Transpose of a matrix => a21(new) = a12(old).
+    RectangularMatrix transpose() const {
+      // Early returns.
+      if(n_entries() == 1) return *this;
+      if(m_rows == 1 || m_columns == 1) {
+        RectangularMatrix result(*this);
+        std::swap(result.m_rows, result.m_columns);
+        return result;
+      }
+      // Main work.
+      RectangularMatrix result(m_columns,m_rows); // Potentially slower but safer...let's just stay in our lane, shall we?
+      for(size_t r = 0; r < m_rows; r++)
+        for(size_t c = 0; c < m_columns; c++)
+          result.at(c + OFFSET_INDEX, r + OFFSET_INDEX) = at(r + OFFSET_INDEX, c + OFFSET_INDEX);
+      return result;
+    }
+    
+  // Comparison operators.
+  public:
+    bool operator==(const RectangularMatrix &other) {
+      if(this == &other) return true;
+      if(order() != other.order()) return false;
+      for(size_t i = 0; i < order(); i++) if(m_entries[i] != other.m_entries[i]) return false;
+      return true;
+    }
+    
+    bool operator!=(const RectangularMatrix &other) const noexcept {
+      return !((*this) == other);
+    }
+    
+  // Transformation operators.
+  public:
+    RectangularMatrix& negate() {
+      for(size_t i = 0; i < n_entries(); i++) m_entries[i] = -m_entries[i];
+      return *this;
+    }
+    
     RectangularMatrix operator-() const {
       RectangularMatrix result(*this);
       for(size_t i = 0; i < n_entries(); i++) result.m_entries[i] = -m_entries[i];
       return result;
-    }
-    
-    RectangularMatrix& negate() {
-      for(size_t i = 0; i < n_entries(); i++) m_entries[i] = -m_entries[i];
-      return *this;
     }
     
     RectangularMatrix& operator+=(const RectangularMatrix &other) {
@@ -218,17 +264,6 @@ class RectangularMatrix {
       return *this;
     }
     
-    bool operator==(const RectangularMatrix &other) {
-      if(this == &other) return true;
-      if(order() != other.order()) return false;
-      for(size_t i = 0; i < order(); i++) if(m_entries[i] != other.m_entries[i]) return false;
-      return true;
-    }
-    
-    bool operator!=(const RectangularMatrix &other) const noexcept {
-      return !((*this) == other);
-    }
-    
     // Mirror about x-axis.
     RectangularMatrix flip_h() const {
       if(m_rows == 1) return *this;
@@ -246,23 +281,6 @@ class RectangularMatrix {
       for(size_t c = 0; c < m_columns / 2; c++)
         for(size_t r = 0; r < m_rows; r++)
           std::swap(result.m_entries[r*m_columns + c], result.m_entries[r*m_columns + (m_columns - 1 - c)]);
-      return result;
-    }
-    
-    // Transpose of a matrix => a21(new) = a12(old).
-    RectangularMatrix transpose() const {
-      // Early returns.
-      if(n_entries() == 1) return *this;
-      if(m_rows == 1 || m_columns == 1) {
-        RectangularMatrix result(*this);
-        std::swap(result.m_rows, result.m_columns);
-        return result;
-      }
-      // Main work.
-      RectangularMatrix result(m_columns,m_rows); // Potentially slower but safer...let's just stay in our lane, shall we?
-      for(size_t r = 0; r < m_rows; r++)
-        for(size_t c = 0; c < m_columns; c++)
-          result.at(c + OFFSET_INDEX, r + OFFSET_INDEX) = at(r + OFFSET_INDEX, c + OFFSET_INDEX);
       return result;
     }
     
@@ -289,17 +307,43 @@ class RectangularMatrix {
       return result;
     }
     
-    void print(std::ostream &os) const {
-      os << "\n{\n";
+    RectangularMatrix rotate_acw() const {
+      // Early returns.
+      if(n_entries() == 1) return *this;
+      if(m_columns == 1) {
+        RectangularMatrix result(*this);
+        std::swap(result.m_columns, result.m_rows);
+        return result;
+      }
+      if(m_rows == 1) {
+        RectangularMatrix result(*this);
+        std::swap(result.m_columns, result.m_rows);
+        for(size_t i = 0; i < n_entries() / 2; i++) std::swap(result.m_entries[i], result.m_entries[m_columns - 1 - i]); // Reverse the order.
+        return result;
+      }
+      // Main work.
+      RectangularMatrix result(m_columns, m_rows);
+      for(size_t r = 0; r < result.m_rows; r++)
+        for(size_t c = 0; c < result.m_columns; c++)
+          result.at(r + OFFSET_INDEX, c + OFFSET_INDEX) = at(c + OFFSET_INDEX, m_columns - 1 - r + OFFSET_INDEX);
+      return result;
+    }
+    
+    void print(std::ostream &os, const std::string &start = "\n") const {
+      os << start;
+      os << "{\n";
       for(size_t r = 0; r < m_rows; r++)
         for(size_t c = 0; c < m_columns; c++)
           os << at(r + OFFSET_INDEX, c + OFFSET_INDEX) << (c < m_columns - 1 ? ", " : (r < m_rows - 1 ? "\n" : "\n}"));
     }
+    
     friend std::ostream& operator<<(std::ostream &os, const RectangularMatrix &matrix) {
       matrix.print(os);
       return os;
     }
     
+  // Destructor.
+  public:
     // Destructor....RAII.
     ~RectangularMatrix() {
       delete[] m_entries;
@@ -316,12 +360,14 @@ void swap(RectangularMatrix<T> &lfs, RectangularMatrix<T> &rhs) noexcept {
   lfs.swap(rhs);
 }
 
+// TODO - SquareMatrix.
+
 }
 
 int main() {
-  mat::RectangularMatrix<int> my_matrix({{1,2},{1,1}});
-  // my_matrix.print(std::cout);
-  std::cout << my_matrix << "\n\n";
+  mat::RectangularMatrix<int> my_matrix({{2,2},{1,1}});
+  my_matrix.print(std::cout, ""); // <- This works too.
+  std::cout << my_matrix;
   std::cout << my_matrix.rotate_cw();
   return 0;
 }
