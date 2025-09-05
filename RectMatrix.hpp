adceabc3d5fab957dcddf331ac38224c559511a3
Vishesh Meena <visheshmeena47@gmail.com>
2025-09-05 17:20:24 +0530

    Made constructors
    Made constructors for matrix classes

---
+++ RectMatrix.hpp
@@ -0,0 +1,167 @@
+#pragma once
+
+#include <stdint.h>
+#include <limits>
+#include <initializer_list>
+#include <algorithm>
+
+namespace Matrix {
+
+    typedef enum class Direction : char {
+        VERTICAL, HORIZONTAL
+    } DR;
+
+    typedef enum class Modifier : char {
+        EXTEND, SHRINK, SAME_SIZE_MUST
+    } MD;
+
+    template <typename Type>
+    class Rect {
+        private:
+            size_t m_rows, m_columns;
+            Type *m_data;
+        
+        public:
+            Rect() {
+                m_rows = 0;
+                m_columns = 0;
+                m_data = new Type[0]();
+            }
+
+            Rect(const Type * const array, const size_t rows, const size_t columns): m_rows(rows), m_columns(columns) {
+                if (rows * columns == 0) {
+                    m_rows = 0;
+                    m_columns = 0;
+                }
+                m_data = new Type[rows * columns];
+                std::copy(array, array + rows * columns, m_data);
+            }
+
+            Rect(const std::initializer_list<Type> &il, const size_t rows, const size_t columns): m_rows(rows), m_columns(columns) {
+                if (rows * columns == 0) {
+                    m_rows = 0;
+                    m_columns = 0;
+                }
+                if (rows * columns != il.size()) m_data = new Type[il.size()]();
+                else m_data = new Type[il.size()];
+                size_t min = std::min(rows * columns, il.size());
+                std::copy(il.begin(), il.end(), m_data);
+            }
+
+            Rect(const std::initializer_list<Type> &il, const Matrix::Direction dir = Matrix::DR::HORIZONTAL) {
+                switch (dir) {
+                    case Matrix::DR::HORIZONTAL:
+                        m_rows = il.size() != 0;
+                        m_columns = il.size();
+                        break;
+                    case Matrix::DR::VERTICAL:
+                        m_rows = il.size();
+                        m_columns = il.size() != 0;
+                        break;
+                }
+                m_data = new Type[il.size()];
+                std::copy(il.begin(), il.end(), m_data);
+            }
+            
+            Rect(const std::initializer_list<std::initializer_list<Type>> &ill, const  Matrix::Modifier modi = Matrix::MD::EXTEND) {
+                size_t i = 0;
+                auto FALLBACK = [&]() -> void {
+                    m_rows = 0;
+                    m_columns = 0;
+                    m_data = new Type[0]();
+                };
+                switch (modi) {
+                    case Matrix::MD::EXTEND:
+                        size_t max_size = 0;
+                        for (const std::initializer_list<Type> &il : ill) 
+                            if (il.size() > max_size) max_size = il.size();
+                        if (max_size == 0) {
+                            FALLBACK();
+                            return;
+                        }
+                        m_rows = ill.size();
+                        m_columns = max_size;
+                        m_data = new Type[ill.size() * max_size]();
+                        size_t row_num = 0;
+                        for (const std::initializer_list<Type> &il: ill) {
+                            std::copy(il.begin(), il.begin() + il.size(), m_data + (i++) * max_size);
+                        }
+                        break;
+                    case Matrix::MD::SHRINK:
+                        size_t min_size = std::numeric_limits<size_t>::max();
+                        for (const std::initializer_list<Type> &il : ill) 
+                            if (il.size() < min_size) min_size = il.size();
+                        m_rows = ill.size();
+                        m_columns = min_size;
+                        if (min_size == 0) {
+                            FALLBACK();
+                            return;
+                        }
+                        m_data = new Type[ill.size() * min_size];
+                        for (const std::initializer_list<Type> &il: ill) {
+                            std::copy(il.begin(), il.begin() + min_size, m_data + (i++) * min_size);
+                        }
+                        break;
+                    case Matrix::MD::SAME_SIZE_MUST:
+                        if (ill.size() != 0) {
+                            size_t col_size = ill[0].size();
+                            for (const std::initializer_list<Type> &il: ill) {
+                                if (il.size() != col_size) break;
+                            }
+                            if (i != ill.size()) throw std::logic_error("Every row doesn't contain the same number of columns. Err: at Rect construction.");
+                            m_rows = ill.size();
+                            m_columns = ill[0].size();
+                            m_data = new Type[m_rows * m_columns];
+                            i = 0;
+                            for (const std::initializer_list<Type> &il : ill)
+                                for (const Type &tp : il)
+                                    m_data[i++] = tp;
+                        }
+                        else {
+                            FALLBACK();
+                            return;
+                        }
+                }
+            }
+
+            Rect(const Rect &other): m_rows(other.m_rows), m_columns(other.m_columns) {
+                m_data = new Type[m_rows * m_columns];
+                std::copy(other.m_data, other.m_data + m_rows * m_columns, m_data);
+            }
+
+            Rect(Rect &&other): m_rows(other.m_rows), m_columns(other.m_columns), m_data(other.m_data) {
+                other.m_data = nullptr;
+                other.m_rows = 0;
+                other.m_columns = 0;
+            }
+
+            Rect& operator=(const Rect &other) {
+                if (this == &other) return *this;
+                m_rows = other.m_rows;
+                m_columns = other.m_columns;
+                if (m_data != nullptr) delete[] m_data;
+                m_data = new Type[m_columns * m_rows];
+                std::copy(other.m_data, other.m_data + m_rows * other.m_columns, m_data);
+                return *this;
+            }
+
+            Rect& operator=(Rect &&other) {
+                if (this == &other) return *this;
+                if (m_data != nullptr) delete[] m_data;
+                m_rows = other.m_rows;
+                m_columns = other.m_columns;
+                m_data = other.m_data;
+                other.m_data = nullptr;
+                other.m_rows = 0;
+                other.m_columns = 0;
+                return *this;
+            }
+
+            // TO-DO: Under Construction
+
+            ~Rect() {
+                delete[] m_data;
+            }
+    };
+
+}
