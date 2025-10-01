1cc3afded1936519891ee695f3f8bce7b7e82329
Vishesh Meena <visheshmeena47@gmail.com>
2025-10-01 23:56:00 +0530

    bug fixes and more util

--- Matrix.hpp
+++ Matrix.hpp
@@ -77,21 +77,26 @@ namespace math::matrix
             size_t column() const noexcept {
                 return m_columns;
             }
-
-            void set_row(const size_t row) noexcept {
-                m_rows = row;
-            }
-
-            void set_column(const size_t column) noexcept {
-                m_columns = column;
-            }
-
+    
         public:
+            void flip() noexcept {
+                std::swap(m_rows, m_columns);
+            }
+
             void swap(Order &other) noexcept {
                 std::swap(m_rows, other.m_rows);
                 std::swap(m_columns, other.m_columns);
             }
-    };
+
+            void set_row(const size_t row) noexcept {
+                m_rows = row;
+            }
+    
+            void set_column(const size_t column) noexcept {
+                m_columns = column;
+            }
+
+        };
 
     template <typename T, size_t row_num>
     class RowIterator {
@@ -132,19 +137,19 @@ namespace math::matrix
         
         public:
             RowIterator operator++() noexcept {
-                RowIterator prev(ptr++);
+                RowIterator prev(m_ptr++);
                 return prev;
             }
             RowIterator operator++(int) noexcept {
-                ++ptr;
+                ++m_ptr;
                 return *this;
             }
             RowIterator operator--() noexcept {
-                RowIterator prev(ptr--);
+                RowIterator prev(m_ptr--);
                 return prev;
             }
             RowIterator operator--(int) noexcept {
-                --ptr;
+                --m_ptr;
                 return *this;
             }
 
@@ -156,15 +161,15 @@ namespace math::matrix
                 return RowIterator(m_ptr - sub);
             }
             RowIterator operator+=(size_t add) noexcept {
-                ptr += add;
+                m_ptr += add;
                 return *this;
             }
             RowIterator operator-=(size_t sub) noexcept {
-                ptr -= sub;
+                m_ptr -= sub;
                 return *this;
             }
             long long operator-(const RowIterator &other) const noexcept {
-                return (ptr - other.m_ptr);
+                return (m_ptr - other.m_ptr);
             }
     };
 
@@ -174,27 +179,27 @@ namespace math::matrix
             T *m_data;
             size_t m_col_size;
         public:
-            T &at(size_t index) {
-                if (index >= col_size) throw std::invalid_argument("Index is greater than number of elements in the row and hence can't be accessed.");
+            T &at(const size_t index) {
+                if (index >= m_col_size) throw std::out_of_range("Index is greater than number of elements in the row and hence can't be accessed.");
                 return m_data[index];
             }
-            const T &at(size_t index) const {
-                if (index >= col_size) throw std::invalid_argument("Index is greater than number of elements in the row and hence can't be accessed.");
+            const T &at(const size_t index) const {
+                if (index >= m_col_size) throw std::out_of_range("Index is greater than number of elements in the row and hence can't be accessed.");
                 return m_data[index];
             }
 
         public:
-            RowIterator begin() noexcept {
-                return RowIterator(m_data);
+            RowIterator<T, row_num> begin() noexcept {
+                return RowIterator<T, row_num>(m_data);
             }
-            RowIterator end() noexcept {
-                return RowIterator(m_data + m_col_size);
+            RowIterator<T, row_num> end() noexcept {
+                return RowIterator<T, row_num>(m_data + m_col_size);
             }
-            const RowIterator begin() const noexcept {
-                return RowIterator(m_data);
+            const RowIterator<T, row_num> begin() const noexcept {
+                return RowIterator<T, row_num>(m_data);
             }
-            const RowIterator end() const noexcept {
-                return RowIterator(m_data + m_col_size);
+            const RowIterator<T, row_num> end() const noexcept {
+                return RowIterator<T, row_num>(m_data + m_col_size);
             }
     };
 
@@ -202,6 +207,11 @@ namespace math::matrix
         zero, possible_garbage
     };
     using CAR = ConstructAllocateRule;
+
+    enum class ConstructOrientationRule {
+        horizontal, vertical, main_diagonal, off_diagonal
+    };
+    using COR = ConstructOrientationRule;
 }
 
 namespace math
@@ -218,14 +228,26 @@ namespace math
             Matrix(const math::matrix::Order &order, const math::matrix::CAR construct_rule = math::matrix::CAR::possible_garbage) : m_order(order) {
                 const size_t row = m_order.row();
                 const size_t column = m_order.column();
-                m_data = ::operator new[](sizeof(T*) * row);
+                m_data = static_cast<T**>(::operator new[](sizeof(T*) * row));
                 for (size_t i = 0; i < row; i++) {
-                    m_data[i] = ::operator new[](sizeof(T) * column);
+                    m_data[i] = static_cast<T*>(::operator new[](sizeof(T) * column));
                 }
                 if (construct_rule == math::matrix::CAR::possible_garbage) {
                     if constexpr (std::is_trivially_constructible_v<T>) return;
+                    else if constexpr (std::is_default_constructible_v<T>) {
+                        T *end = nullptr;
+                        for (size_t i = 0; i < row; i++) {
+                            try {
+                                end = std::uninitialized_default_construct_n(m_data[i], column);
+                            } catch(...) {
+                                math::matrix::impl::destroy_data<T>(m_data, i, end, row, column);
+                                throw;
+                            }
+                        }
+                        return;
+                    }
                     if (math::helper::zero_vals.exists_of<T>()) {
-                        T zero_val(math::helper::zero_vals.get_of<T>());
+                        const T zero_val(math::helper::zero_vals.get_of<T>());
                         T *end = nullptr;
                         for (size_t i = 0; i < row; i++) {
                             try {
@@ -258,7 +280,7 @@ namespace math
                             try {
                                 end = std::uninitialized_fill_n(m_data[i], column, zero_val);
                             } catch(...) {
-                                math::matrix::impl::destroy_data(m_data, i, end, row, column);
+                                math::matrix::impl::destroy_data<T>(m_data, i, end, row, column);
                                 throw;
                             }
                         }
@@ -271,10 +293,10 @@ namespace math
             Matrix(const math::matrix::Order &order, const T &to_copy) : m_order(order) {
                 const size_t row = m_order.row();
                 const size_t column = m_order.column();
-                m_data = ::operator new[](sizeof(T*) * row);
+                m_data = static_cast<T**>(::operator new[](sizeof(T*) * row));
                 T *end = nullptr;
                 for (size_t i = 0; i < row; i++) {
-                    m_data[i] = ::operator new[](sizeof(T) * column);
+                    m_data[i] = static_cast<T*>(::operator new[](sizeof(T) * column));
                 }
                 for (size_t i = 0; i < row; i++) {
                     try {
@@ -291,10 +313,10 @@ namespace math
             Matrix(const T **const data, const math::matrix::Order &order) : m_order(order) {
                 const size_t row = m_order.row();
                 const size_t column = m_order.column();
-                m_data = ::operator new[](sizeof(T*) * row);
+                m_data = static_cast<T**>(::operator new[](sizeof(T*) * row));
                 T* end = nullptr;
                 for (size_t i = 0; i < row; i++) {
-                    m_data[i] = ::operator new[](sizeof(T) * column);
+                    m_data[i] = static_cast<T*>(::operator new[](sizeof(T) * column));
                 }
                 for (size_t i = 0; i < row; i++) {
                     try {
@@ -307,6 +329,82 @@ namespace math
             }
             Matrix(const T **const data, const size_t row, const size_t column) : Matrix(data, math::matrix::Order(row, column)) {}
 
+            Matrix(const T *const data, const size_t size, math::matrix::COR construct_rule = math::matrix::COR::horizontal) {
+                T *end = nullptr;
+                bool zero_exists = (math::helper::zero_vals.exists_of<T>());
+                switch (construct_rule) {
+                    case math::matrix::COR::horizontal :
+                        m_order = math::matrix::Order(1, size);
+                        m_data = static_cast<T**>(::operator new[](sizeof(T*)));
+                        try {
+                            end = std::uninitialized_copy(data, data + size, m_data[0]);
+                        } catch(...) {
+                            math::matrix::impl::destroy_data<T>(m_data, 0, end, m_order.row(), m_order.column());
+                            throw;
+                        }
+                        return;
+                    case math::matrix::COR::vertical :
+                        m_order = math::matrix::Order(size, 1);
+                        m_data = static_cast<T**>(::operator new[](sizeof(T*) * size));
+                        for (size_t i = 0; i < size; i++) {
+                            m_data[i] = static_cast<T*>(::operator new[](sizeof(T)));
+                            try {
+                                std::construct_at(m_data[i], data[i]);
+                            } catch(...) {
+                                math::matrix::impl::destroy_data<T>(m_data, i, m_data[i], m_order.row(), m_order.column());
+                                throw;
+                            }
+                        }
+                        return;
+                    case math::matrix::COR::main_diagonal :
+                        m_data = static_cast<T**>(::operator new[](sizeof(T*) * size));
+                        m_order = math::matrix::Order(size, size);
+                        for (size_t i = 0; i < size; i++) {
+                            m_data[i] = static_cast<T*>(::operator new[](sizeof(T) * size));
+                            try {
+                                if (zero_exists) {
+                                    end = std::uninitialized_fill_n(m_data[i], i, math::helper::zero_vals.get_of<T>());
+                                    end = std::construct_at(m_data[i] + i, data[i]);
+                                    end = std::uninitialized_fill_n(m_data[i] + i + 1, size - 1 - i, math::helper::zero_vals.get_of<T>());
+                                }
+                                else if constexpr (std::is_default_constructible_v<T>) {
+                                    end = std::uninitialized_value_construct_n(m_data[i], i);
+                                    end = std::construct_at(m_data[i] + i, data[i]);
+                                    end = std::uninitialized_value_construct_n(m_data[i] + i + 1, size - 1 - i);
+                                }
+                                else throw std::logic_error("Cannot construct the matrix for this type because neither zero value is stored and neither is it default constructible.");
+                            } catch(...) {
+                                math::matrix::impl::destroy_data<T>(m_data, i, end, m_order.row(), m_order.column());
+                                throw;
+                            }
+                        }
+                        return;
+                    case math::matrix::COR::off_diagonal :
+                        m_data = static_cast<T**>(::operator new[](sizeof(T*) * size));
+                        m_order = math::matrix::Order(size, size);
+                        for (size_t i = 0; i < size; i++) {
+                            m_data[i] = static_cast<T*>(::operator new[](sizeof(T) * size));
+                            try {
+                                if (zero_exists) {
+                                    end = std::uninitialized_fill_n(m_data[i], size - 1 - i, math::helper::zero_vals.get_of<T>());
+                                    end = std::construct_at(m_data[i] + size - 1 - i, data[i]);
+                                    end = std::uninitialized_fill_n(m_data[i] + size - i, i, math::helper::zero_vals.get_of<T>());
+                                }
+                                else if constexpr (std::is_default_constructible_v<T>) {
+                                    end = std::uninitialized_value_construct_n(m_data[i], size - 1 - i);
+                                    end = std::construct_at(m_data[i] + i, data[i]);
+                                    end = std::uninitialized_value_construct_n(m_data[i] + i + 1, i);
+                                }
+                                else throw std::logic_error("Cannot construct the matrix for this type because neither zero value is stored and neither is it default constructible.");
+                            } catch(...) {
+                                math::matrix::impl::destroy_data<T>(m_data, i, end, m_order.row(), m_order.column());
+                                throw;
+                            }
+                        }
+                        return;
+                }
+            }
+        
         public:
             Matrix(const Matrix &other) : Matrix(other.m_data, other.m_order) {}
             Matrix(Matrix &&other) noexcept : m_order(other.m_order), m_data(other.m_data) {
@@ -328,13 +426,34 @@ namespace math
 
         public:
             ~Matrix() {
-                math::matrix::impl::destroy_data(m_data, m_order.m_rows(), m_data[m_order.m_rows()] + m_order.m_columns(), m_order.m_rows(), m_order.m_columns());
+                math::matrix::impl::destroy_data(m_data, m_order.row(), m_data[m_order.row()] + m_order.column(), m_order.row(), m_order.column());
+            }
+
+        public:
+            T &operator()(const size_t row, const size_t column) noexcept {
+                return m_data[row][column];
+            }
+            const T &operator()(const size_t row, const size_t column) const noexcept {
+                return m_data[row][column];
+            }
+            T &at(const size_t row, const size_t column) {
+                if ((row >= m_order.row()) || (column >= m_order.column())) throw std::out_of_range("Provided index does not exist within the bounds of this matrix.");
+                return m_data[row][column];
+            }
+            const T &at(const size_t row, const size_t column) const {
+                if ((row >= m_order.row()) || (column >= m_order.column())) throw std::out_of_range("Provided index does not exist within the bounds of this matrix.");
+                return m_data[row][column];
             }
 
         public:
             void swap(Matrix &other) noexcept {
                 m_order.swap(other.m_order);
                 std::swap(m_data, other.m_data);
+            }
+
+        public:
+            const math::matrix::Order &order() const noexcept {
+                return m_order;
             }
     };
 }
--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include "Math.hpp"
 #include <iostream>
 
-using namespace math::helper;
-
 int main(void) {
-    zero_vals.store_of(0);
-    zero_vals.store_of(10.0f);
-    math::matrix::Order order(30, 30);
-    std::cout << zero_vals.get_of<int>() << ' ' << zero_vals.get_of<float>() << std::endl;
-    std::cout << (order == math::matrix::Order(90, 20)) << std::endl;
-    std::cout << sizeof(decltype(order)) << std::endl;
-    std::cout << sizeof(math::Matrix<int>) << std::endl;
-    std::cout << alignof(math::Matrix<int>) << std::endl;
-    math::Matrix<int> int_mat;
-    const math::matrix::Order &rorder = int_mat.order();
-    std::cout << int_mat.data() << std::endl;
-    std::cout << rorder.row() << ' ' << rorder.column() << std::endl;
+    int data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    math::Matrix<int> my_matrix(data, 10, math::matrix::ConstructOrientationRule::main_diagonal);
+    std::cout << my_matrix.order().row() << ' ' << my_matrix.order().column() << std::endl;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) 
+            std::cout << my_matrix.at(i, j) << ' ';
+        std::cout << '\n';
+    }
 }
